f83d1b1953adb88e51b3e1be684a95333cb03fce
JUNAID B S <junaidkummil73@gmial.com>
2021-03-31 00:05:25 +0530

    Version1.1

---
+++ app/src/main/java/com/cybarz/driverapp/History.java
@@ -0,0 +1,13 @@
+package com.cybarz.driverapp;
+
+public class History {
+     public  String tranid,from,to;
+    History(String tranId,String from,String to){
+        this.tranid=tranId;
+        this.from=from;
+        this.to=to;
+
+    }
+
+
+}
